12d03e9020ab9a06208ffc5a7d6cb9f5aefb7c09
xuewenke <379435450@qq.com>
2020-07-13 19:45:26 +0800

    前缀树的定义

---
+++ src/main/java/com/x/atrs/leetcode/tree/Trie.java
@@ -0,0 +1,16 @@
+
+/*
+ * @Copyright 2014-2020 云安宝 (www.yunanbao.com.cn).
+ *
+ * 注意：本内容仅限于深圳云安宝科技有限公司内部传阅，禁止外泄以及用于其他商业目的
+ *
+ */
+
+package com.x.atrs.leetcode.tree;
+
+/**
+ * @author xuewenke
+ * @since 2020/7/13 7:45 下午
+ */
+public class Trie {
+}
