33061c272ee166bb7b272f52d534830884a229f9
xuewenke <379435450@qq.com>
2020-07-28 10:30:58 +0800

    两个有序数组找中位数。

--- src/main/java/com/x/atrs/leetcode/array/FindMedianSortedArrays.java
+++ src/main/java/com/x/atrs/leetcode/array/FindMedianSortedArrays.java
@@ -15,51 +15,70 @@ import lombok.experimental.UtilityClass;
  * 你可以假设 nums1 和 nums2 不会同时为空。
  * 中位数就是有序的数组，中间的数。偶位数，取平均值。
  * 思路：
- * 1.把两个数组合并后排序，找中间的数。
- * 2. 先确定好中位数的下标，然后两个数据对比，找出中位数。
+ * 1. 已知两个数组为有序的数组，则可以使用归并排序的方法，合并成一个有序的数组
+ *
  */
 @UtilityClass
 public class FindMedianSortedArrays {
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] mergeNums = merge(nums1, nums2);
+        return singleNums(mergeNums);
+    }
+
+    /**
+     * 合并两个有序的数组
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    private int[] merge(int[] nums1, int[] nums2) {
         if (nums1.length == 0) {
-            return singleNums(nums2);
+            return nums2;
         }
 
         if (nums2.length == 0) {
-            return singleNums(nums1);
+            return nums1;
         }
 
         int allLength = nums1.length + nums2.length;
-        // 余数
-        int remainder = allLength % 2;
-        // 整除
-        int divide = allLength / 2;
-//        int cursor = 1;
-        int nums1Index = 0;
-        int nums2Index = 0;
-        int singleTarget = 0;
-        for (int i = 0; i <= divide; i++) {
-            if (nums1[nums1Index] <= nums2[nums2Index]) {
-                int temp = nums1Index++;
-                if (i == divide) {
-                    singleTarget = nums1[temp];
-                }
-            } else {
-                int temp = nums2Index++;
-                if (i == divide) {
-                    singleTarget = nums1[temp];
-                }
 
-            }
-        }
+        int[] mergeNums = new int[allLength];
 
-        if (remainder == 0) {
-            return (nums1[nums1Index] + nums2[nums2Index]) / 2D;
-        } else {
-            return singleTarget;
-        }
+        return mergeNums;
     }
+
+
+//    public void merge(int[] nums1, int[] nums2) {
+//        int m = nums1.length;
+//        int n = nums2.length;
+//        // Make a copy of nums1.
+//        int [] nums1_copy = new int[m];
+//        System.arraycopy(nums1, 0, nums1_copy, 0, m);
+//
+//        // Two get pointers for nums1_copy and nums2.
+//        int p1 = 0;
+//        int p2 = 0;
+//
+//        // Set pointer for nums1
+//        int p = 0;
+//
+//        // Compare elements from nums1_copy and nums2
+//        // and add the smallest one into nums1.
+//        while ((p1 < m) && (p2 < n)) {
+//            nums1[p++] = (nums1_copy[p1] < nums2[p2]) ? nums1_copy[p1++] : nums2[p2++];
+//        }
+//
+//        // if there are still elements to add
+//        if (p1 < m) {
+//            System.arraycopy(nums1_copy, p1, nums1, p1 + p2, m + n - p1 - p2);
+//        }
+//        if (p2 < n) {
+//            System.arraycopy(nums2, p2, nums1, p1 + p2, m + n - p1 - p2);
+//        }
+//    }
+
 
     private double singleNums(int[] nums) {
         // 余数
