132706693d2a5af7b6dcd3fd7e6aa98f21b2c42e
xuewenke <379435450@qq.com>
2020-08-06 00:54:35 +0800

    init  demo

--- src/main/java/com/x/atrs/leetcode/tree/TreeNode.java
+++ src/main/java/com/x/atrs/leetcode/tree/TreeNode.java
@@ -3,7 +3,11 @@ package com.x.atrs.leetcode.tree;
 
 import lombok.Data;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
+ *  二叉树
  * @author xuewenke
  * @since 2020/8/4 1:18 下午
  */
@@ -17,4 +21,44 @@ public class TreeNode {
         this.val = x;
     }
 
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> collect = new LinkedList<>();
+        helper(collect, root);
+        return collect;
+    }
+
+    /**
+     * 二叉树 中序遍历
+     *
+     * @param collect
+     * @param node
+     */
+    private void helper(List<Integer> collect, TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        collect.add(node.val);
+        if (node.left != null) {
+            helper(collect, node.left);
+        }
+
+        if (node.right != null) {
+            helper(collect, node.right);
+        }
+    }
+
+
+    /**
+     *  最大深度
+     * @param root
+     * @return
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftMax = maxDepth(root.left);
+        int rightMax = maxDepth(root.right);
+        return Math.max(leftMax, rightMax) + 1;
+    }
 }
