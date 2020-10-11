13511dc5fa2db441bce2aee5d5d2c2035d9578db
薛文科 <xuewenke@qianxin.com>
2020-10-11 17:10:55 +0800

    demo

--- src/test/java/com/x/atrs/util/JavaTest.java
+++ src/test/java/com/x/atrs/util/JavaTest.java
@@ -1,16 +1,7 @@
 
-/*
- * @Copyright 2014-2020 云安宝 (www.yunanbao.com.cn).
- *
- * 注意：本内容仅限于深圳云安宝科技有限公司内部传阅，禁止外泄以及用于其他商业目的
- *
- */
 
 package com.x.atrs.util;
 
-import org.testng.annotations.Test;
-
-import java.text.DecimalFormat;
 
 /**
  * @author xuewenke
@@ -19,10 +10,7 @@ import java.text.DecimalFormat;
 public class JavaTest {
 
 
-    @Test
-    public void testName() {
-        DecimalFormat format = new DecimalFormat("#.00");
+    public static void main(String[] args) {
 
-        System.out.println(format.format(87.3134));
     }
 }
